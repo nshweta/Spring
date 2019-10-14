1be5b940c0cca092ab840146aa9d4f982e799a65
nshweta <shweta.nivargi@gmail.com>
2019-10-14 15:20:35 +0530

    Add files via upload

---
+++ CustomerManager/src/com/myproject/customer/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.myproject.customer;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+}
